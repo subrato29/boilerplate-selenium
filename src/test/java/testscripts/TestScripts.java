b1ca8bc154c767d61562e2594fc1b073c3c4930b
subrato29 <subratosarkar29@gmai.com>
2022-01-31 08:20:20 -0500

    updated testscript and pom

--- src/test/java/testscripts/TestScripts.java
+++ src/test/java/testscripts/TestScripts.java
@@ -9,7 +9,7 @@ import com.web.base.DriverScript;
 
 public class TestScripts extends DriverScript {
     @Test
-    public void flow_AFB_Create_User_FS_AT043() throws InterruptedException, IOException {
+    public void loginTest() throws InterruptedException, IOException {
         String testCaseID = "TC001";
         if (isTestCaseRunnable(testCaseID)) {
             Keywords.login();
